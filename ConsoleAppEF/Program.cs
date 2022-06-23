c941f5a0548e98815d03faab2a6163347b0aa0e7
Roberto C. Castaño <elrobertcharle@outlook.com>
2022-06-23 17:56:43 -0400

    ok

--- ConsoleAppEF/Program.cs
+++ ConsoleAppEF/Program.cs
@@ -4,10 +4,5 @@ Console.WriteLine("Hello, World!");
 
 using (var context = new ConsoleAppEF.DAL.Academy())
 {
-    context.Courses!.Add(new ConsoleAppEF.DAL.Course
-    {
-        Name = "C1"
-    });
 
-    context.SaveChanges();
 }
